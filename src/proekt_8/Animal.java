e57602cc6d1b1d28645467ef5ec6b151f04aa4f9
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 09:32:16 +0100

    vezba27

---
+++ src/proekt_8/Animal.java
@@ -0,0 +1,8 @@
+package proekt_8;
+public class Animal {
+	void bark(){
+		System.out.println("Woof-Woof");
+
+	}
+
+}
---
+++ src/proekt_8/MyClass.java
@@ -0,0 +1,10 @@
+package proekt_8;
+
+public class MyClass {
+	public static void main(String[] args) {
+			Animal dog=new Animal();
+			dog.bark();
+
+		}
+
+	}
